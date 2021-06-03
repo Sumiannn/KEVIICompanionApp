0c4ba3796364489306cec7867f98a4a4e62de986
Sumiannn <e0524717@u.nus.edu>
2021-06-03 21:48:38 +0800

    new home page

--- lib/Screens/email_signup.dart
+++ lib/Screens/email_signup.dart
@@ -300,3 +300,29 @@ class _EmailSignUpState extends State<EmailSignUp> {
     roomController.dispose();
   }
 }
+class pathPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+    paint.color = KELightRed;
+    paint.style = PaintingStyle.fill;
+    var path = Path();
+    path.moveTo(0, size.height * 0.4);
+    path.quadraticBezierTo(size.width * 0.35, size.height * 0.40,
+        size.width * 0.58, size.height * 0.6);
+    path.quadraticBezierTo(size.width * 0.72, size.height * 0.8,
+        size.width * 0.92, size.height * 0.8);
+    path.quadraticBezierTo(
+        size.width * 0.98, size.height * 0.8, size.width, size.height * 0.82);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
+  }
+}
