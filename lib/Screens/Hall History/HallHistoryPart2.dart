9a77a136b26e6214cab7e14d0467a05536922827
Chng Lip Kiam <lipkiam@hotmail.com>
2021-06-19 19:29:55 +0800

    new outlook, sliding up panels in hall history pages

--- lib/Screens/Hall History/HallHistoryPart2.dart
+++ lib/Screens/Hall History/HallHistoryPart2.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:keviiapp/colorScheme.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 import '../email_login.dart';
 import '../home.dart';
@@ -10,17 +11,24 @@ class HallHistoryPart2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: bgColor,
-        body: Column(
-          children: <Widget>[
-            Stack(
+        body: SlidingUpPanel(
+            minHeight: 485.0,
+            maxHeight: MediaQuery.of(context).size.height,
+            borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            panelBuilder: (scrollController) => buildSlidingPanel(
+              scrollController: scrollController,
+            ),
+            body: Stack(
               clipBehavior: Clip.none,
               children: [
                 Container(
                   height: MediaQuery.of(context).size.height,
                   width: MediaQuery.of(context).size.width,
-                  child: Image.asset('assets/image/KingEdwardHolneChase.jpg',
+                  child: Image.asset(
+                    "assets/image/KingEdwardHolneChase.jpg",
                     alignment: Alignment.topLeft,
-                    fit: BoxFit.fitWidth,),
+                    fit: BoxFit.fitWidth,
+                  ),
                 ),
                 Positioned(
                   top: 25,
@@ -67,67 +75,90 @@ class HallHistoryPart2 extends StatelessWidget {
                     },
                   ),
                 ),
-                Positioned(
-                  top: 230,
-                  bottom: 0,
-                  left: 0,
-                  right: 0,
-                  child: Container(
-                      height: MediaQuery.of(context).size.height * 0.8,
-                      width: MediaQuery.of(context).size.width,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          topRight: Radius.circular(20),
-                        ),
-                        color: bgColor,
-                      ),
-                      child: ListView(
-                        physics: BouncingScrollPhysics(),
-                        shrinkWrap: true,
-                        scrollDirection: Axis.vertical,
-                        children: [
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Center(
-                            child: Text(
-                              "Holne Chase",
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  fontSize: 26,
-                                  fontWeight: FontWeight.w700,
-                                  color: KERed),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Center(
-                            child: Text(
-                              "Grange Road (1938-1956)",
-                              textAlign: TextAlign.center,
-                              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: KEYellow),
-                            ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.all(20),
-                            padding: EdgeInsets.all(20),
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: KELightRed),
-                            child: Text(
-                              "Female students during the F.M.S. years stayed in a bungalow at Grange Road, quaintly called “Holne Chase”, an all-female hostel. The grounds were beautiful with huge trees and flowering shrubs. There were also tennis and badminton courts.",
-                              textAlign: TextAlign.start,
-                              style: TextStyle(fontSize: 18),
-                            ),
-                          ),
-                        ],
-                      )),
-                ),
               ],
-            )
-          ],
-        ));
+            )));
   }
+
+  Widget buildSlidingPanel({
+    @required ScrollController scrollController,
+  }) =>
+      TabWidget(scrollController: scrollController);
 }
+
+class TabWidget extends StatelessWidget {
+  const TabWidget({
+    Key key,
+    @required this.scrollController,
+  }) : super(key: key);
+  final ScrollController scrollController;
+
+  @override
+  Widget build(BuildContext context) => ListView(
+      padding: EdgeInsets.all(0),
+      controller: scrollController,
+      children: [
+        Positioned(
+          top: 230,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Container(
+              height: MediaQuery.of(context).size.height * 0.8,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+                color: bgColor,
+              ),
+              child: ListView(
+                physics: BouncingScrollPhysics(),
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                children: [
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Center(
+                    child: Text(
+                      "Holne Chase",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.w700,
+                          color: KERed),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Center(
+                    child: Text(
+                      "Grange Road (1938-1956)",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                          color: KEYellow),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(20),
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: KELightRed),
+                    child: Text(
+                      "Female students during the F.M.S. years stayed in a bungalow at Grange Road, quaintly called “Holne Chase”, an all-female hostel. The grounds were beautiful with huge trees and flowering shrubs. There were also tennis and badminton courts.",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ],
+              )),
+        ),
+      ]
+  );
+}
+
