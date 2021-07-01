e5751e04b4fb95e19505f238f7d5638cdd3e2c55
Lippy21 <lipkiam@hotmail.com>
2021-07-01 22:22:04 +0800

    Logout alertdialog

--- lib/HallInfo/CCAInfo.dart
+++ lib/HallInfo/CCAInfo.dart
@@ -79,13 +79,7 @@ class CCAInfo extends StatelessWidget {
                   size: 30,
                 ),
                 onPressed: () {
-                  FirebaseAuth auth = FirebaseAuth.instance;
-                  auth.signOut().then((res) {
-                    Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(builder: (context) => EmailLogIn()),
-                            (Route<dynamic> route) => false);
-                  });
+                  logOutNotice(context);
                 },
               ),
             ),
@@ -165,6 +159,40 @@ class CCAInfo extends StatelessWidget {
           ]
       ),
     );
+  }
+
+  void logOutNotice(BuildContext context) {
+    var alertDialog = AlertDialog(
+      title: Text("Are you sure you want to Log Out?"),
+      actions: <Widget>[
+        FlatButton(
+          onPressed: () {
+            print("Cancel");
+            Navigator.of(context).pop(false);
+          },
+          child: Text('Cancel', style: TextStyle(color: Colors.black),),
+        ),
+        FlatButton(
+          onPressed: () {
+            print("Logout");
+            FirebaseAuth auth = FirebaseAuth.instance;
+            auth.signOut().then((res) {
+              Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => EmailLogIn()),
+                      (Route<dynamic> route) => false);
+            });
+          },
+          child: Text('Logout', style: TextStyle(color: Colors.black),),
+        )
+      ],
+    );
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alertDialog;
+        });
   }
 
 }
